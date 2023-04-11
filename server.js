f029298861a7f92d0ac2e349b1df8f6b87ac6fc4
Kimmy <kimberlieguillaume@gmail.com>
2023-04-11 19:58:21 -0400

    added start prompt questions

--- server.js
+++ server.js
@@ -17,3 +17,80 @@
  * WHEN I choose to update an employee role
  * THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
  */
+
+//Dependencies
+const mysql = require("mysql2/promise");
+const express = require("express");
+const inquirer = require("inquirer");
+const db = require("./db");
+const fs = require("fs");
+const cTable = require("console.table");
+
+//Set up Express app
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+//Set-up Middleware
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//Initial function at start
+function init(){
+    startPrompt();
+}
+
+//Start Prompt Questions 
+function startPrompt() {
+    inquirer.prompt({
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update An Employee Role"]
+
+    }).then( answer => {
+        let answer = answer.menu; 
+        switch (answer.menu) {
+            case "View All Departments":
+                viewAllDepartments();
+                break;
+            case "View All Roles":
+                viewAllRoles();
+                break;
+            case "View All Employees":
+                viewAllEmployees();
+                break;
+            case "Add A Department":
+                addDepartment();
+                break;
+            case "Add A Role":
+                addRole();
+                break;
+            case "Add An Employee":
+                addEmployee();
+                break;
+            case "Update An Employee Role":
+                updateEmployeeRole();
+                break;    
+        }
+
+    })
+};
+
+// View all departments 
+function viewAllDepartments() {
+    db.allDepartments()
+        .then(([rows]) => {
+            let departments = rows
+            console.table(departments);
+        })
+        .then(() => startPrompt());
+}
+// View all roles
+// View all employees
+// Add a department
+// Add a role
+// Add an employee
+// Update an employee role
+
+//Call to start app
+init();
